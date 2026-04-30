2e566014bbf8c8b35e169c19af26b9f876d602bc
forwot <176093262+forwot@users.noreply.github.com>
2026-04-30 16:33:35 +0800

    add: category btns click UI

--- src/index.js
+++ src/index.js
@@ -23,21 +23,31 @@ function clearContent(){
 
 export { createHtmlElement, contentContainer };
 
+// BUTTON CLICKED STYLING
+function setActiveBtn(clickedBtn){
+    const catBtns = document.querySelectorAll('.cat-container button');
+    catBtns.forEach(btn => btn.classList.remove('active-btn'));
+    clickedBtn.classList.add('active-btn');
+}
+
 // EVENT LISTENERS
 const inboxBtn = document.querySelector('button.inbox');
 inboxBtn.addEventListener('click', ()=>{
     clearContent()
     renderInbox();
+    setActiveBtn(inboxBtn);
 })
 
 const todayBtn = document.querySelector('button.today');
 todayBtn.addEventListener('click', ()=>{
     clearContent()
     renderToday();
+    setActiveBtn(todayBtn);
 })
 
 const weekBtn = document.querySelector('button.week');
 weekBtn.addEventListener('click', ()=>{
     clearContent()
     renderWeek();
+    setActiveBtn(weekBtn);
 })

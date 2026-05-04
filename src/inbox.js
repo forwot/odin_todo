005271e6f0f01304fd81e8b8c1c616434ab93696
forwot <176093262+forwot@users.noreply.github.com>
2026-05-04 14:49:15 +0800

    add: task btn date input field

--- src/inbox.js
+++ src/inbox.js
@@ -81,6 +81,39 @@ function addTask(name, taskContainer){
         e.stopPropagation();
         checkBox.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>checkbox-marked-circle-outline</title><path d="M20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4C12.76,4 13.5,4.11 14.2,4.31L15.77,2.74C14.61,2.26 13.34,2 12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12M7.91,10.08L6.5,11.5L11,16L21,6L19.59,4.58L11,13.17L7.91,10.08Z" /></svg>`;
     })
+
+    date.addEventListener('click',(e)=>{
+        e.stopPropagation();
+
+        const oldDate = date.textContent;
+        
+        date.textContent = '';
+        const dateInput = createHtmlElement('input')
+        dateInput.type = 'date' 
+
+        dateInput.addEventListener('click', (e) => e.stopPropagation());
+        dateInput.focus();
+        
+        dateInput.addEventListener('change', (e => {
+            if (dateInput.value){
+                date.textContent = dateInput.value;
+            }
+            else{
+                date.textContent = 'No date';
+            }
+        }));
+
+        dateInput.addEventListener('blur', (e) => {
+            if(!dateInput.value){
+                date.textContent = oldDate || 'No date';
+            }
+            else{
+                date.textContent = dateInput.value;
+            }
+        });
+        
+        date.appendChild(dateInput);
+    })
 }
 
 export { render as renderProjectContent }
